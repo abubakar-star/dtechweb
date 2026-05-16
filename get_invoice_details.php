28bfc2fa82357273ef493925209deae74611ac8a
abubakar-star <115552981+abubakar-star@users.noreply.github.com>
2026-05-17 01:43:52 +0300

    Update get_invoice_details.php

--- get_invoice_details.php
+++ get_invoice_details.php
@@ -7,22 +7,13 @@ if (!isset($_SESSION['user_id'])) {
     exit;
 }
 
-// DB connection
+$host = $_ENV['MYSQLHOST'];
+$port = $_ENV['MYSQLPORT'];
+$dbname = $_ENV['MYSQLDATABASE'];
+$username = $_ENV['MYSQLUSER'];
+$password = $_ENV['MYSQLPASSWORD'];
 
-$servername = "localhost";
-$db_username = "root";
-$db_password = "";
-$dbname     = "dlink_network";
-
-// DB connection
-/*
-$servername = "sql313.infinityfree.com";
-$db_username = "if0_39741603";
-$db_password = "mkala3771";
-$dbname     = "if0_39741603_dlink_network";
-*/
-
-$conn = new mysqli($servername, $db_username, $db_password, $dbname);
+$conn = new mysqli($host, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
