0d8daf7227224de93871c30ccf5e9e7cde822842
abubakar-star <115552981+abubakar-star@users.noreply.github.com>
2026-05-17 01:38:39 +0300

    Update check_queued.php

--- check_queued.php
+++ check_queued.php
@@ -1,10 +1,12 @@
 <?php
-$conn = new mysqli(
-    "localhost",
-    "root",
-    "",
-    "dlink_network"
-);
+
+$host = $_ENV['MYSQLHOST'];
+$port = $_ENV['MYSQLPORT'];
+$dbname = $_ENV['MYSQLDATABASE'];
+$username = $_ENV['MYSQLUSER'];
+$password = $_ENV['MYSQLPASSWORD'];
+
+$conn = new mysqli($host, $username, $password, $dbname, $port);
 
 $res = $conn->query("
     SELECT id, username 
