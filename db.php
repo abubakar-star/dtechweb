e6f4c4aa7300cd978feffdd4498611cc7db4d400
abubakar-star <115552981+abubakar-star@users.noreply.github.com>
2026-05-20 20:54:34 +0300

    Update db.php

--- db.php
+++ db.php
@@ -9,6 +9,12 @@ $password = $_ENV['MYSQLPASSWORD'];
 $conn = new mysqli($host, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+
+    error_log(
+        "Database connection failed: " .
+        $conn->connect_error
+    );
+
+    die("Database connection failed");
 }
 ?>
