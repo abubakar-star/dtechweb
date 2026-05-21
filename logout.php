8ecb2fddf4a8f76bce543ef85ed569cd3853739a
abubakar-star <115552981+abubakar-star@users.noreply.github.com>
2026-05-21 04:09:12 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,21 @@
 <?php
 session_start();
+include 'includes/logger.php';
+
+$user_id = $_SESSION['user_id'] ?? null;
+
+if ($user_id) {
+
+    createLog(
+        $conn,
+        'authentication',
+        'User Logout',
+        'User logged out successfully',
+        'info',
+        $user_id
+    );
+
+}
 
 // Clear all session variables
 session_unset();
@@ -7,7 +23,7 @@ session_destroy();
 
 // Delete cookies (both user and token)
 if (isset($_COOKIE["remember_user"])) {
-    $conn = new mysqli("localhost", "root", "", "dlink_network");
+    include 'db.php';
     $username = $_COOKIE["remember_user"];
     $conn->query("UPDATE users SET remember_token=NULL WHERE username='$username'");
     $conn->close();
