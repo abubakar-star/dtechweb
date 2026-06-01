21feed585a70be04c84427a084be9037acae2222
abubakar-star <115552981+abubakar-star@users.noreply.github.com>
2026-06-01 19:45:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -188,17 +188,13 @@ if ($hour >= 5 && $hour < 12) {
              </svg>';
 }
 
-echo "<pre>";
-print_r([
-    'REMOTE_ADDR' => $_SERVER['REMOTE_ADDR'] ?? '',
-    'HTTP_X_FORWARDED_FOR' => $_SERVER['HTTP_X_FORWARDED_FOR'] ?? '',
-    'HTTP_X_REAL_IP' => $_SERVER['HTTP_X_REAL_IP'] ?? '',
-    'HTTP_CF_CONNECTING_IP' => $_SERVER['HTTP_CF_CONNECTING_IP'] ?? ''
-]);
-echo "</pre>";
-exit;
-
-$ip = $_SERVER['REMOTE_ADDR'];
+if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+    $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = trim(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'])[0]);
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'] ?? 'Unknown';
+}
 
 $user_id = $_SESSION['user_id']; 
 
