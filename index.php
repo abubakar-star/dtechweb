75486e1302dfeb93607960301d46511e4ceae7ee
abubakar-star <115552981+abubakar-star@users.noreply.github.com>
2026-06-05 01:45:06 +0300

    Add manifest and theme color meta tags

--- index.php
+++ index.php
@@ -262,6 +262,8 @@ $conn->close();
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>D-LINK NETWORK</title>
   <link rel="icon" href="tt.png" type="x-icon" />
+  <link rel="manifest" href="/manifest.json">
+<meta name="theme-color" content="#2563eb">
   <script src="https://cdn.tailwindcss.com"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
