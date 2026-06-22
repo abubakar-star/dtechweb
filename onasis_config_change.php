92b61ba23ba7ee2f9e590cce6a3ef23711256090
abubakar-star <115552981+abubakar-star@users.noreply.github.com>
2026-06-22 12:36:36 +0300

    Update onasis_config_change.php

--- onasis_config_change.php
+++ onasis_config_change.php
@@ -1,6 +1,6 @@
 <?php
 
-$ONASIS_SECRET_KEY = "pay_6ceecf07b938d143bc8ff745b3037eaaaafa2064579a4cfc";
+$ONASIS_SECRET_KEY = "pay_d68b738ac6c4a5ce846855853a2d481025fb561f72c7b953";
 
 $ONASIS_BASE_URL = "https://pay.onasis.tech";
 
